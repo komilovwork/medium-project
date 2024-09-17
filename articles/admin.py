7bd1c6e717f4991b70e26aeef8b4d01a00a6e65b
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-09-17 20:07:39 +0500

    cors

--- articles/admin.py
+++ articles/admin.py
@@ -1,2 +1,6 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Topic, Article, FAQ
 
-# Register your models here.
+
+admin.site.register([Topic, Article, FAQ])
